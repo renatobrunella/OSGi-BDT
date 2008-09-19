6c855acd2552c3c760036aa672320db1558a205f
brunella <brunella@5b6af2aa-2c7c-4d48-b9e4-037c2e344d0e>
2008-09-19 14:45:50 +0000

    Initial

---
+++ trunk/uk.co.brunella.osgi.bdt.example/uk.co.brunella.osgi.bdt.example.test.dictionary/src/uk/co/brunella/osgi/bdt/example/test/dictionary/Activator.java
@@ -0,0 +1,16 @@
+package uk.co.brunella.osgi.bdt.example.test.dictionary;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import uk.co.brunella.osgi.bdt.osgitestrunner.OSGiTestCaseUtils;
+
+public class Activator implements BundleActivator {
+
+  public void start(BundleContext context) throws Exception {
+    OSGiTestCaseUtils.run(context, new GermanDictionaryTestCase(context));
+  }
+
+  public void stop(BundleContext context) throws Exception {
+  }
+}
