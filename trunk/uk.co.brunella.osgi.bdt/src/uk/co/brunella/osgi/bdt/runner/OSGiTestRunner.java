a784ef6b0721f522516af1de4d483ef4e69f7e94
brunella <brunella@5b6af2aa-2c7c-4d48-b9e4-037c2e344d0e>
2009-05-02 11:00:19 +0000

    Imports cleaned up

--- trunk/uk.co.brunella.osgi.bdt/src/uk/co/brunella/osgi/bdt/runner/OSGiTestRunner.java
+++ trunk/uk.co.brunella.osgi.bdt/src/uk/co/brunella/osgi/bdt/runner/OSGiTestRunner.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.lang.reflect.Method;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
