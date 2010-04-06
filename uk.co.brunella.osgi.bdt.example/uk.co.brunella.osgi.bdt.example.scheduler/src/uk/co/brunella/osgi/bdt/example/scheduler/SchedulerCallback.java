f29ff3cbf318c20f516705cec46911bfcd5704a6
brunella <brunella@5b6af2aa-2c7c-4d48-b9e4-037c2e344d0e>
2010-04-06 09:21:59 +0000

    Branched out production code stream for new 3.x development on trunk

--- uk.co.brunella.osgi.bdt.example/uk.co.brunella.osgi.bdt.example.scheduler/src/uk/co/brunella/osgi/bdt/example/scheduler/SchedulerCallback.java
+++
@@ -1,8 +0,0 @@
-package uk.co.brunella.osgi.bdt.example.scheduler;
-
-public interface SchedulerCallback {
-
-  static final String SCHEDULE_PERIOD_IN_MILLISECOND = "PERIOD_IN_MILLISECOND";
-  
-  void callback();
-}
